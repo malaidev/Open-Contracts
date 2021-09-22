0f70a029d674853ce3342ef08e791e71908df34e
john082Coder <johnmoody9527@gmail.com>
2021-09-22 22:00:27 +0530

    Minor

--- contracts/Deposit.sol
+++ contracts/Deposit.sol
@@ -1,10 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.7 <0.9.0;
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
-// import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-// import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./TokenList.sol";
 import "./util/Address.sol";
 
