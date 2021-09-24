eaadd6a94eaef5fb0f6cd95e63fcad4f3e96158f
john082Coder <johnmoody9527@gmail.com>
2021-09-24 13:03:28 +0800

    update functions

--- contracts/TokenList.sol
+++ contracts/TokenList.sol
@@ -1,34 +1,38 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.7 <0.9.0;
+pragma solidity >= 0.8.7 < 0.9.0;
 
 import "./AccessRegistry.sol";
 import "./util/Address.sol";
 
-contract TokenList {
-  bytes32 internal adminTokenList;
-
-  struct TokenRegistry {
-    mapping(bytes32 => address) tokenAddress; // maps a symbol to its address
-    mapping(bytes32 => uint256) decimals; // maps a symbol to its decimals
-    mapping(bytes32 => uint256) tokenIndex; // index helps you verify if this token exists or not
-    bytes32[] symbols;
+contract TokenList{
+  struct TokenData{
+    byte32 symbol;
+    address tokenAddress;
+    uint decimals;
+    uint chainId;
+    bool isSupported;
   }
-  
-  mapping(bytes32 => TokenRegistry) supportedTokens;
+
+  byte32[] allSymbols;
+
+  mapping(byte32 >= TokenData) public tokenPointer;
 
   event TokenSupportAdded(bytes32 indexed _symbol,uint256 _decimals,address indexed _tokenAddress,uint256 indexed _timestamp);
   event TokenSupportRemoved(bytes32 indexed _symbol,uint256 _decimals,address indexed _tokenAddress,uint256 indexed _timestamp);
-  constructor() {
-    // address  constant BNB = 0x02822e968856186a20fEc2C824D4B174D0b70502;
-    // address  constant STACK = 0x04DF6e4121c27713ED22341E7c7Df330F56f289B;
-    // address  constant USDC = 0x9780881Bf45B83Ee028c4c1De7e0C168dF8e9eEF;
+ 
 
-    // implement the default token support to some of the tokens.
-  }
+  function isTokenSupported(bytes32  _symbol) external view returns (bool)	{
+		_isTokenSupported(_symbol);
+		return true;
+	}
 
+	function _isTokenSupported(bytes32  _symbol) internal view {
+		TokenData storage tokenData  = tokenPointer[_symbol];
+		require(tokenData.isSupported == true, "Token is not supported");
+	}
 
 // ADD A NEW TOKEN SUPPORT
-  function addTokenSupport(bytes32 _symbol,uint256 _decimals,address _tokenAddress) external returns (bool success) {
+  function addTokenSupport(bytes32 _symbol,uint256 _decimals,address _tokenAddress) external returns (bool) {
     
     _isTokenSupported(_symbol);
     _addTokenSupport(_symbol, _decimals, _tokenAddress);
@@ -36,36 +40,43 @@ contract TokenList {
     emit TokenSupportAdded(_symbol,_decimals,_tokenAddress,block.timestamp
     );
 
-    return bool(success);
+    return bool(true);
   }
-
-  function _isTokenSupported(bytes32 _symbol) internal view {
-    require(supportedTokens[_symbol].tokenIndex[_symbol] != 0,"This token is already supported");
-    this;
-  }
-
   function _addTokenSupport( bytes32 _symbol,uint256 _decimals,address _tokenAddress) internal {
-    TokenRegistry storage tokenRegistry = supportedTokens[_symbol];
+    
 
-    tokenRegistry.tokenAddress[_symbol] = _tokenAddress;
-    tokenRegistry.decimals[_symbol] = _decimals;
-    tokenRegistry.symbols.push(_symbol); // adds the token symbol to the tokenregistry
-    tokenRegistry.tokenIndex[_symbol] = tokenRegistry.symbols.length; 
+    TokenData storage tokenData = tokenPointer[_symbol];
+    
+    tokenData.symbol = _symbol;
+    tokenData.tokenAddress = _tokenAddress;
+    tokenData.decimals = _decimals;
+    tokenData.isSupported = true;
+
+    allSymbols.push(symbol);
   }
 
-  function removeTokenSupport(bytes32 _symbol) external returns(bool success) {}
-  function _removeTokenSupport(bytes32 _symbol) internal {}
+  function removeTokenSupport(bytes32 _symbol) external returns(bool) {
+    _removeTokenSupport(_symbol);
+    return bool(true);
+  }
+  function _removeTokenSupport(bytes32 _symbol) internal {
+   
+    delete tokenPointer[_symbol];
 
+  }
 
-// temporary disabling a token support could come in hand during the events a
-// token is exploited or somethnig like that
-  // function tempDisableTokenSupport(bytes32 _symbol) external returns(bool success)  {}
+  function updateTokenSupport(byte32 _symbol, uint256 _decimals,address _tokenAddress) external returns(bool){
+    _updateTokenSupport(_symbol, _decimals, _tokenAddress);
+    return bool(true);
+  }
+  function _updateTokenSupport(byte32 _symbol, uint256 _decimals,address _tokenAddress) internal{
+
+    TokenData storage tokenData = tokenPointer[_symbol];
+    
+    tokenData.symbol = _symbol;
+    tokenData.tokenAddress = _tokenAddress;
+    tokenData.decimals = _decimals;
+    tokenData.isSupported = true;
+  }
+
 }
-
-// Tokenlist contract holds the list of suppported tokens, base and
-// non-base.All the other contracts - deposit, lender, comptroller inherit from
-// the tokenlist contract.
-
-
-// Instead of a single public function, I will create an external and internal
-// function for free flow of interactions
