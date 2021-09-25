813a932dbca945913f55b52d09ad1332182fd096
john082Coder <johnmoody9527@gmail.com>
2021-09-25 09:13:42 +0800

    TokeList.sol fix errs.

--- contracts/TokenList.sol
+++ contracts/TokenList.sol
@@ -13,13 +13,14 @@ contract TokenList{
   }
 
   bytes32[] allSymbols;
-
   mapping (bytes32 => bool) public isSymbolExist;
   mapping (bytes32=>uint256) symbolIndex;
   mapping (bytes32 => TokenData) public tokenPointer;
 
   event TokenSupportAdded(bytes32 indexed _symbol,uint256 _decimals,address indexed _tokenAddress,uint256 indexed _timestamp);
-  event TokenSupportRemoved(bytes32 indexed _symbol,uint256 _decimals,address indexed _tokenAddress,uint256 indexed _timestamp);
+  event TokenSupportUpdated(bytes32 indexed _symbol,uint256 _decimals,address indexed _tokenAddress,uint256 indexed _timestamp);
+  event TokenSupportRemoved(bytes32 indexed _symbol, uint256 indexed _timestamp);
+
  
   function isTokenSupported(bytes32  _symbol) external view returns (bool)	{
 		_isTokenSupported(_symbol);
@@ -27,7 +28,6 @@ contract TokenList{
 	}
 
 	function _isTokenSupported(bytes32  _symbol) internal view {
-	
 		require(isSymbolExist[_symbol] == true, "Token is not supported");
 	}
 
@@ -37,9 +37,7 @@ contract TokenList{
     _isTokenSupported(_symbol);
     _addTokenSupport(_symbol, _decimals, _tokenAddress);
 
-    emit TokenSupportAdded(_symbol,_decimals,_tokenAddress,block.timestamp
-    );
-
+    emit TokenSupportAdded(_symbol,_decimals,_tokenAddress,block.timestamp);
     return bool(true);
   }
   function _addTokenSupport( bytes32 _symbol,uint256 _decimals,address _tokenAddress) internal {
@@ -52,11 +50,11 @@ contract TokenList{
     allSymbols.push(_symbol);
     isSymbolExist[_symbol] = true;
     symbolIndex[_symbol] = allSymbols.length-1;
-
   }
 
   function removeTokenSupport(bytes32 _symbol) external returns(bool) {
     _removeTokenSupport(_symbol);
+    emit TokenSupportRemoved(_symbol, block.timestamp);
     return bool(true);
   }
   function _removeTokenSupport(bytes32 _symbol) internal {
@@ -80,6 +78,7 @@ contract TokenList{
   
   function updateTokenSupport(bytes32 _symbol, uint256 _decimals,address _tokenAddress) external returns(bool){
     _updateTokenSupport(_symbol, _decimals, _tokenAddress);
+    emit TokenSupportUpdated(_symbol,_decimals,_tokenAddress,block.timestamp);
     return bool(true);
   }
   function _updateTokenSupport(bytes32 _symbol, uint256 _decimals,address _tokenAddress) internal{
