dbafdda9dc4d4ced33ae2ee0077deade34f7397b
john082Coder <johnmoody9527@gmail.com>
2022-01-27 15:40:32 +0800

    add util folder

---
+++ contracts/util/Pausable.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.1;
+
+import "./Context.sol";
+
+abstract contract Pausable is Context {
+
+	bool isReentrant = false;
+    bool private isPaused;
+    event PauseState(address indexed _pauser, bool isPaused);
+
+    constructor() {
+        isPaused = false;
+    }
+
+    modifier whenNotPaused() {
+        require(!_paused(), "Paused status");
+        _;
+    }
+                                                                                                                                                                                                                                              
+    modifier whenPaused() {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+        require(_paused(), "Not paused status");
+        _;
+    }
+
+    function _pause() internal virtual whenNotPaused {
+        isPaused = true;
+        emit PauseState(_msgSender(), true);
+    }
+
+    function _unpause() internal virtual whenPaused {
+        isPaused = false;
+        emit PauseState(_msgSender(), false);
+    }
+
+    function _paused() internal virtual view returns(bool) {
+        return isPaused;
+    }
+
+    function _checkPauseState() internal view {
+        require(isPaused == false,"The contract is paused. Transfer functions are temporarily disabled");
+    }
+
+    modifier nonReentrant() {
+		require(isReentrant == false, "Re-entrant alert!");
+		isReentrant = true;
+		_;
+		isReentrant = false;
+	}
+}
