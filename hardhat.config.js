a650cac13b56f9edd6b19a628e8d5c884682709b
john082Coder <johnmoody9527@gmail.com>
2022-02-01 16:27:14 +0530

    Added hh verify in config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 const { task } = require("hardhat/config");
 const { getSelectors, FacetCutAction } = require('./scripts/libraries/diamond.js')
 const utils = require('ethers').utils
-
+require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
