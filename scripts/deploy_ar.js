cc59fe999237e534d2ace1f4040bccd931259cff
john082Coder <johnmoody9527@gmail.com>
2022-02-02 13:00:18 +0530

    Added msg.sender

--- scripts/deploy_ar.js
+++ scripts/deploy_ar.js
@@ -2,7 +2,7 @@ const { ethers, upgrades } = require("hardhat");
 
 async function main() {
   const AccessRegistry = await ethers.getContractFactory("AccessRegistry");
-  const AccessRegistry = await upgrades.deployProxy(AccessRegistry, address(this), {
+  const AccessRegistry = await upgrades.deployProxy(AccessRegistry, msg.sender, {
     initializer: "initialize",
 });
   await AccessRegistry.deployed();
