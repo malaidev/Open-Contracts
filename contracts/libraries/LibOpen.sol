2f4454189c133a64ef6c157ec4f06f4ddf91622e
john082Coder <johnmoody9527@gmail.com>
2022-02-06 09:45:26 +0800

    fix _getLatestPrice get back

--- contracts/libraries/LibOpen.sol
+++ contracts/libraries/LibOpen.sol
@@ -615,11 +615,10 @@ library LibOpen {
 
 // =========== OracleOpen Functions =================
 	function _getLatestPrice(bytes32 _market) internal view returns (uint) {
-		// AppStorageOpen storage ds = diamondStorage();
-		// require(ds.pairAddresses[_market] != address(0), "ERROR: Invalid pair address");
-		// ( , int price, , , ) = AggregatorV3Interface(ds.pairAddresses[_market]).latestRoundData();
-		// return uint256(price);
-		return 1;
+		AppStorageOpen storage ds = diamondStorage();
+		require(ds.pairAddresses[_market] != address(0), "ERROR: Invalid pair address");
+		( , int price, , , ) = AggregatorV3Interface(ds.pairAddresses[_market]).latestRoundData();
+		return uint256(price);
 	}
 
 	function _getFairPrice(uint _requestId) internal view returns (uint) {
