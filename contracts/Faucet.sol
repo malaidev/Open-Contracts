24a2f95589337c862ff19d0d37a22e68bd616d50
john082Coder <johnmoody9527@gmail.com>
2022-02-11 10:35:02 +0530

    [TEST] Mint reduced to 5mn

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -80,7 +80,7 @@ contract Faucet {
         _;
     }
 }
-// // SPDX-License-Identifier: MIT
+
 // pragma solidity 0.8.1; 
 // interface BEP20 {
 //     function transfer(address to, uint256 value) external returns (bool);
